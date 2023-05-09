33edd709717eee3bef43cc5f9fea7b87240f6186
hoanghuydo787 <hoanghuydo787@gmail.com>
2023-05-09 22:40:03 +0700

    update save model in AI

--- systemAI/automatic_system.py
+++ systemAI/automatic_system.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 import math
 import pickle
+import csv
 
 def entropy(labels):
     n = len(labels)
@@ -75,135 +76,19 @@ def predict(tree, sample):
         # Traverse the right subtree if the sample's feature value is greater than or equal to the threshold
         return predict(tree['right_subtree'], sample)
 
+def build_and_save_dt(fileName):
+    with open(fileName + '.csv', 'r') as f:
+        data = list(csv.reader(f, delimiter=','))
+        f.close()
+    X1, y1 = [], []
+    for ele in data:
+        X1 += [[int(i) for i in ele[:-1]]]
+        y1 += [int(ele[-1])]
+    tree1 = build_tree(X1, y1)
+    water_pump_dt_path = fileName + '.pkl'
+    with open(water_pump_dt_path, 'wb') as f:
+        pickle.dump(tree1, f)
+        f.close()
 
-sample = [28, 73, 150]  # Sample with 4 features
-# temperature, soil moisture, luminance
-X1 = [
-    [28, 75, 100],
-    [28, 60, 10000],
-    [29, 40, 4000],
-    [32, 20, 200],
-    [33, 60, 5000],
-
-    [28, 85, 2000],
-    [28, 90, 1600],
-    [30, 95, 3000],
-    [32, 98, 5000],
-    [31, 87, 2000],
-
-    [28, 90, 1200],
-    [28, 92, 800],
-    [29, 88, 600],
-    [30, 86, 1400],
-    [31, 98, 1000],
-    
-    [25, 95, 3000],
-    [25, 25, 200],
-    [25, 45, 1000],
-    [25, 65, 1500],
-    [25, 75, 6000],
-]
-y1 = [
-    1,
-    1,
-    1,
-    1,
-    1,
-
-    1,
-    1,
-    1,
-    1,
-    1,
-    
-    0,
-    0,
-    0,
-    0,
-    0,
-    
-    0,
-    0,
-    0,
-    0,
-    0
-]
-tree1 = build_tree(X1, y1)
-predicted_class = predict(tree1, sample)
-print('Predicted class:', predicted_class)
-water_pump_dt_path = 'water_pump_dt.pkl'
-with open(water_pump_dt_path, 'wb') as f:
-    pickle.dump(tree1, f)
-    f.close()
-
-# temperature, soil moisture, luminance 
-X2 = [
-    [28, 75, 1900],
-    [29, 63, 10000],
-    [30, 43, 4500],
-    [31, 20, 2500],
-    [32, 78, 5800],
-    
-    [28, 82, 10000],
-    [28, 90, 10000],
-    [29, 99, 4000],
-    [32, 88, 2000],
-    [33, 96, 5000],
-
-    [28, 85, 1000],
-    [28, 90, 1400],
-    [30, 95, 900],
-    [32, 98, 500],
-    [31, 87, 200],
-
-    [26, 90, 1200],
-    [26, 92, 800],
-    [25, 88, 600],
-    [20, 86, 1400],
-    [18, 98, 1000],
-     
-    [26, 90, 2200],
-    [26, 92, 3800],
-    [25, 88, 6000],
-    [20, 86, 1600],
-    [18, 98, 1900]
-]
-# control tarpaulin
-y2 = [
-    0,
-    0,
-    0,
-    0,
-    0,
-
-    0,
-    0,
-    0,
-    0,
-    0,
-
-    1,
-    1,
-    1,
-    1,
-    1,
-
-    1,
-    1,
-    1,
-    1,
-    1,
-
-    1,
-    1,
-    1,
-    1,
-    1
-]
-tree2 = build_tree(X2, y2)
-predicted_class = predict(tree2, sample)
-print('Predicted class:', predicted_class)
-motor_dt_path = 'motor_dt.pkl'
-with open(motor_dt_path, 'wb') as f:
-    pickle.dump(tree2, f)
-    f.close()
+build_and_save_dt('water_pump_dt')
+build_and_save_dt('motor_dt')
